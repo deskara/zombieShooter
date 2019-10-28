bdcd6075423f41fa717424d2c9c7d572faf75a1a
deskara <55700741+deskara@users.noreply.github.com>
2019-10-28 17:25:18 +0000

    Zombies can be slain
    They also look and move towards you.

---
+++ Assets/Scripts/healthSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+public class healthSystem : MonoBehaviour
+{
+    public int health = 10;
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        onDamaged.Invoke(health);
+        if (health < 1)
+        {
+            onDie.Invoke();
+            //As there are no listeners for the onDie I'm currently just destroying the Zombie.
+            Destroy(gameObject);
+        }
+    }
+}
