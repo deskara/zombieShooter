2ee95717ea6fe66153470cd22ab77270fd21cd57
deskara <55700741+deskara@users.noreply.github.com>
2019-10-28 17:52:32 +0000

    Player can now take damage
    When spelling collides with debugging. Whoops...

--- Assets/Scripts/healthSystem.cs
+++ Assets/Scripts/healthSystem.cs
@@ -5,6 +5,7 @@ public class OnDamagedEvent : UnityEvent<int> { }
 public class healthSystem : MonoBehaviour
 {
     public int health = 10;
+    public int damage = 5;
     public UnityEvent onDie;
     public OnDamagedEvent onDamaged;
     public void TakeDamage(int damage)
@@ -18,4 +19,13 @@ public class healthSystem : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("An attempt to make the player take damage was made");
+        //This makes the other entity run the "take damage" method
+        collision.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+    }
+
+   
 }
