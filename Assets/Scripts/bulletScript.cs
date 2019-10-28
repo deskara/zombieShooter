67a4a85d78660284fae92cb83948dde868013811
deskara <55700741+deskara@users.noreply.github.com>
2019-10-28 16:54:48 +0000

    Added Bullet Script
    Bug was added, now unable to shoot

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+    public int damage = 1;
+    private void Start()
+    {
+        //This gets the rigidbody component and adds force to it
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //This makes the other entity run the "take damage" method
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
