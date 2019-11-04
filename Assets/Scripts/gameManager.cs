5f07f67e414e96ed445cff9ab16f523fc7080f9c
deskara <55700741+deskara@users.noreply.github.com>
2019-11-04 17:45:11 +0000

    Added a basic main menu

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ZombieShooterLevel1");
+    }
+    public void EndGame()
+    {
+        //SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        //SceneManager.LoadScene("Main Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();  
+    }
+}
