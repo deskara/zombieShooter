0ba34073cbaa15db2633232f85b52449257d1136
deskara <55700741+deskara@users.noreply.github.com>
2019-11-04 16:43:19 +0000

    Added Blood explosions to Zombies upon damage and death

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public float adjustmentAngle = 270;
+    public void Spawn()
+    {
+        Vector3 rotationInDegrees = transform.eulerAngles;
+        rotationInDegrees.z += adjustmentAngle;
+        Quaternion rotationInRadians = Quaternion.Euler(rotationInDegrees);
+        Instantiate(prefabToSpawn, transform.position, rotationInRadians);
+    }
+}
