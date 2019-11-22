22ba2ebef4c3adf491f5f9441ee05f33af48dadc
deskara <55700741+deskara@users.noreply.github.com>
2019-11-22 16:47:57 +0000

    Added a TutorialEvents script to handle changes in the tutorial.
    Currently the event for firing triggers but the event for moving fails to trigger.

---
+++ Assets/Scripts/tutorialEvents.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class tutorialEvents : MonoBehaviour
+{
+    public UnityEvent playerMoved;
+    public UnityEvent playerShot;
+    GameObject player = GameObject.FindWithTag("Player");
+    Transform initialPosition;
+    private void Start()
+    {
+        initialPosition = player.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButton(0) | Input.GetMouseButton(1))
+        {
+            playerShot.Invoke();
+        }
+
+        if (initialPosition != player.transform)
+        {
+            playerMoved.Invoke();
+        }
+        
+    }
+}
