c024573f78d54bc34395af0d493c1c3ad8e9e4d5
deskara <55700741+deskara@users.noreply.github.com>
2019-11-23 16:52:52 +0000

    Added a way to quit without dying
    The player can press escape to get a button to press which allows them to quit. The game is currently not paused when the button is up and holding down the button causes it to go on and off constantly.

---
+++ Assets/Scripts/escapeMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class escapeMenu : MonoBehaviour
+{
+    public UnityEvent addMenu,removeMenu;
+    bool currentMenuStatus = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            if (currentMenuStatus == false){
+                addMenu.Invoke();
+                currentMenuStatus = true;
+            }
+            else
+            {
+                removeMenu.Invoke();
+                currentMenuStatus = false;
+            }
+
+        }
+        
+    }
+}
