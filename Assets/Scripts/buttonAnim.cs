f11781a8df49c7913fab623513cc1e55b985e5c2
deskara <morganjgorle@gmail.com>
2019-11-25 15:29:30 +0000

    Added an animation for when the player hovers over the start button

---
+++ Assets/Scripts/buttonAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAnim : MonoBehaviour
+{
+    private Animator buttonAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void buttonEnter()
+    {
+        buttonAnimator.SetBool("isHovering", true);
+
+    }
+    public void buttonexit()
+    {
+        buttonAnimator.SetBool("isHovering", false);
+    }
+}
