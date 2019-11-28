18e9cf4393b9389a42c24c37bd2141e7ee33a6c8
deskara <55700741+deskara@users.noreply.github.com>
2019-11-28 19:32:19 +0000

    Began working on making coins collectable.

---
+++ Assets/Scripts/coinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.SendMessage("pickupCoin");
+        Destroy(gameObject);
+
+    }
+}
