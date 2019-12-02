ed9091fc7a50e03b2775daae92b37f9fb1e496c0
deskara <55700741+deskara@users.noreply.github.com>
2019-12-02 16:44:52 +0000

    Added dialogue when the player dies.

---
+++ Assets/Scripts/playerDeathHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class playerDeathHandler : MonoBehaviour
+{
+    public UnityEvent mousePressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) | Input.GetMouseButtonDown(1))
+        {
+            Time.timeScale = 1;
+            mousePressed.Invoke();
+
+
+        }
+        
+    }
+}
