3f0616de3356fc44c7faa57a0ca7443d4e2434c1
deskara <55700741+deskara@users.noreply.github.com>
2019-12-02 16:54:52 +0000

    Player can now enter the homebase

---
+++ Assets/Scripts/enterBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class enterBase : MonoBehaviour
+{
+    public UnityEvent playerInBase;
+    public void baseEntered()
+    {
+        playerInBase.Invoke();
+
+    }
+}
---
+++ Assets/Scripts/playerCollisions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollisions : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //This makes the other entity run the "pickUp" method if it has it
+        collision.transform.SendMessage("pickUp", SendMessageOptions.DontRequireReceiver);
+        //This makes the other entity run the "enterBase method if it has it
+        collision.transform.SendMessage("baseEntered", SendMessageOptions.DontRequireReceiver);
+
+    }
+}
