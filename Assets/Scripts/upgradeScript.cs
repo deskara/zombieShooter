631d92e615345e84ce582399457800b214956b9e
deskara <55700741+deskara@users.noreply.github.com>
2019-12-09 17:27:35 +0000

    Made the upgrade button actually effect damage

--- Assets/Scripts/upgradeScript.cs
+++ Assets/Scripts/upgradeScript.cs
@@ -20,15 +20,17 @@ public class upgradeScript : MonoBehaviour
 
     public void purchaseDamageUpgrade()
     {
-        if(PlayerPrefs.GetInt("Coins") >= PlayerPrefs.GetInt("damage") * 10)
+        if(PlayerPrefs.GetInt("Coins") >= PlayerPrefs.GetInt("damage") * 5)
         {
             PlayerPrefs.SetInt("Coins",PlayerPrefs.GetInt("Coins") - (PlayerPrefs.GetInt("damage") * 5));
+            PlayerPrefs.SetInt("damage", PlayerPrefs.GetInt("damage") + 1);
             coinText.text = string.Format("Coins: {0}",PlayerPrefs.GetInt("Coins"));
-            damageUpgradeCost.text = string.Format("Costs: {0}", (PlayerPrefs.GetInt("damage") * 5))
+            damageUpgradeCost.text = string.Format("Costs: {0}", (PlayerPrefs.GetInt("damage") * 5));
             vendorDialogue.text = "Thanks for your purchase.";
         }
         else
         {
+            damageUpgradeCost.text = string.Format("Costs: {0}", (PlayerPrefs.GetInt("damage") * 5));
             vendorDialogue.text = "You don't have enough money for that.";
 
 
