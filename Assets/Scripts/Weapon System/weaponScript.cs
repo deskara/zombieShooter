1418e0ba9955239529ee7f2bf5ec93cc76268884
deskara <55700741+deskara@users.noreply.github.com>
2019-12-13 16:38:31 +0000

    Added options for the user and made the timers on the two weapons independent of eachother

--- Assets/Scripts/Weapon System/weaponScript.cs
+++ Assets/Scripts/Weapon System/weaponScript.cs
@@ -4,50 +4,68 @@ public class weaponScript : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform leftBulletSpawn;
     public Transform rightBulletSpawn;
-    public float fireTime = 0.5f;
-    private bool isFiring = false;
-    private void SetFiring()
+    public float rightfireTime = 0.25f;
+    public float leftfireTime = 0.25f;
+    private bool rightisFiring = false;
+    private bool leftisFiring = false;
+    private void SetLeftFiring()
     {
-        isFiring = false;
+        leftisFiring = false;
+    }
+    private void SetRightFiring()
+    {
+        rightisFiring = false;
     }
     //LeftFire handles firing out of the left gun
     private void LeftFire()
     {
-        isFiring = true;
-        Instantiate(bulletPrefab, leftBulletSpawn.position, leftBulletSpawn.rotation);
-        if (GetComponent<AudioSource>() != null)
+        if (!leftisFiring)
         {
-            GetComponent<AudioSource>().Play();
+            leftisFiring = true;
+            Instantiate(bulletPrefab, leftBulletSpawn.position, leftBulletSpawn.rotation);
+            if (GetComponent<AudioSource>() != null)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+            Invoke("SetLeftFiring", leftfireTime);
         }
-        Invoke("SetFiring", fireTime);
+
     }
     //RightFire handles firing out of the right gun
     private void RightFire()
     {
-        isFiring = true;
-        Instantiate(bulletPrefab, rightBulletSpawn.position, rightBulletSpawn.rotation);
-        if (GetComponent<AudioSource>() != null)
+        if (!rightisFiring)
         {
-            GetComponent<AudioSource>().Play();
+            rightisFiring = true;
+            Instantiate(bulletPrefab, rightBulletSpawn.position, rightBulletSpawn.rotation);
+            if (GetComponent<AudioSource>() != null)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+            Invoke("SetRightFiring", rightfireTime);
+
         }
-        Invoke("SetFiring", fireTime);
+
     }
     private void FixedUpdate()
     {
         // 0 is the left mouse button
         if (Input.GetMouseButton(0))
         {
-            if (!isFiring)
+            LeftFire();
+            if(PlayerPrefs.GetString("fireState") == "Both")
             {
-                LeftFire();
+                RightFire();
             }
+
         }
         // 1 is the right mouse button
         else if (Input.GetMouseButton(1)) 
         {
-            if (!isFiring)
+            RightFire();
+            if (PlayerPrefs.GetString("fireState") == "Both")
             {
-                RightFire();
+                LeftFire();
             }
 
         }
---
+++ Assets/Scripts/settingsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settingsScript : MonoBehaviour
+{
+    public Text currentFireState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetString("fireState") != "Singular")
+        {
+            PlayerPrefs.SetString("fireState", "Both");
+            currentFireState.text = "Enabled";
+
+        }
+        else
+        {
+            currentFireState.text = "Disabled";
+        }
+        
+    }
+    public void flipFireState()
+    {
+        if(PlayerPrefs.GetString("fireState") == "Both")
+        {
+            PlayerPrefs.SetString("fireState", "Singular");
+            currentFireState.text = "Disabled";
+
+        }
+        else
+        {
+            PlayerPrefs.SetString("fireState", "Both");
+            currentFireState.text = "Enabled";
+
+        }
+    }
+
+}
