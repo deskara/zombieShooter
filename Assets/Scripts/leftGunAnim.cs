80fba93f4cf5d3ca132fea1937d1c10e3ecf9b7c
deskara <55700741+deskara@users.noreply.github.com>
2019-12-14 15:10:03 +0000

    Seperated the player from the weapon as a pre-requisite for adding different weapons into the game.

---
+++ Assets/Scripts/leftGunAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftGunAnim : MonoBehaviour
+{
+    private Animator leftGun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftGun = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            leftGun.SetBool("isFiring", true);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            if (PlayerPrefs.GetString("fireState") == "Both")
+            {
+                leftGun.SetBool("isFiring", true);
+            }
+            else
+            {
+                leftGun.SetBool("isFiring", false);
+            }
+
+        }
+        else
+        {
+            leftGun.SetBool("isFiring", false);
+        }
+    }
+}
---
+++ Assets/Scripts/rightGunAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightGunAnim : MonoBehaviour
+{
+    private Animator rightGun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightGun = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (PlayerPrefs.GetString("fireState") == "Both")
+            {
+                rightGun.SetBool("isFiring", true);
+            }
+            else
+            {
+                rightGun.SetBool("isFiring", false);
+            }
+
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            rightGun.SetBool("isFiring", true);
+
+        }
+        else
+        {
+            rightGun.SetBool("isFiring", false);
+        }
+    }
+}
