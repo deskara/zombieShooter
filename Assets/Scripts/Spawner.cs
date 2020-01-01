a5e662db8a90f70338efc4d1662cb9e73ecbca87
deskara <55700741+deskara@users.noreply.github.com>
2020-01-01 16:01:37 +0000

    Made it so that the new zombies can be spawned from the spawners at a 5% chance for each.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,11 +6,11 @@ public class Spawner : MonoBehaviour
 {
     public GameObject prefabToSpawn;
     public float adjustmentAngle = 270;
-    public void Spawn()
+    public void Spawn(GameObject objectToSpawn)
     {
         Vector3 rotationInDegrees = transform.eulerAngles;
         rotationInDegrees.z += adjustmentAngle;
         Quaternion rotationInRadians = Quaternion.Euler(rotationInDegrees);
-        Instantiate(prefabToSpawn, transform.position, rotationInRadians);
+        Instantiate(objectToSpawn, transform.position, rotationInRadians);
     }
 }
---
+++ Assets/Scripts/zombieSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieSpawner : Spawner
+{
+    public GameObject standardZombie;
+    public GameObject speedyZombie;
+    public GameObject bulkyZombie;
+    public void spawnZombie()
+    {
+        int randomNumber;
+        randomNumber = Random.Range(0, 100);
+        if(randomNumber < 5)
+        {
+            Spawn(bulkyZombie);
+        }
+        else if(randomNumber < 10)
+        {
+            Spawn(speedyZombie);
+        }
+        else
+        {
+            Spawn(standardZombie);
+        }
+    }
+
+}
