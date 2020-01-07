a5a33567e43750703ce7b81df6aac72ad5b887fe
deskara <55700741+deskara@users.noreply.github.com>
2020-01-07 20:33:28 +0000

    Limited FPS to prevent computer screeching.

---
+++ Assets/Scripts/fpsLimiter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsLimiter : MonoBehaviour
+{
+    //This script limits the FPS of the game
+    //This ensures that the game doesn't have excessive FPS e.g 4300 which can cause problems for user's computers.
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+    }
+}
